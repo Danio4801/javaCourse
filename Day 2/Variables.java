3ba23ecdf4b09ff21ac9d9b1f334568a2baf4ba8
Daniel <117715001+Danio4801@users.noreply.github.com>
2023-10-29 22:32:18 +0100

    Lesson 2 - new code added
    This commit contains:
    - new code containing knowledge of/about variables (not finished yet)

---
+++ Day 2/Variables.java
@@ -0,0 +1,29 @@
+class Variables
+{
+    public static void main(String a[])
+    {
+        System.out.print(5 + 6);
+
+        System.out.println(5 + 6); //ln means new line, so just print it in new line
+        System.out.println(11 + 54);
+
+        int num1 = 3; //assigment 
+        System.out.println(num1);
+
+        int num2 = 5;
+        System.out.println(num1 + num2);
+
+        int result = num1 + num2;
+        System.out.println(result);
+
+
+        //DATA TYPES
+        float num3 = 5.6f;
+        
+        long l = 43242423l; 
+        
+        char c = 'k'; //char accept only a single character and we cant use the "", insted for single characters '' is required;
+        boolean b = true; // we dont use the "" because true or false is a keyword itself
+
+    }
+}
