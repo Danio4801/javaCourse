6875ec93d08f593a85d96fbf3a3c60a56c103ac6
Daniel <117715001+Danio4801@users.noreply.github.com>
2023-11-03 13:42:22 +0100

    Day 6 - new code (part II)
    In this new code (Demo.java) we test and discuss:
    - oop.

---
+++ Day 6/Demo.java
@@ -0,0 +1,38 @@
+class Calculator
+{
+    int a;
+
+    public int add(int n1, int n2)
+    {
+        int r = n1 + n2;
+        return r;
+    }
+}
+
+public class Demo
+{
+    public static void main(String args[])
+    {
+        int num1 = 4;
+        int num2 = 5;
+        
+
+        //!!!!!!!!!
+        //We created the object below   
+        Calculator calc = new Calculator(); 
+        //hey, i have calc class and i want to make object based on this "idea", based on Calcualtor class
+        // it is the referance variable
+
+        // new Calculator();  - this is how to we create the object
+        // new Calculator();  - we want a new object of the design Calculator
+        // Calculator calc - we are assigning new object of the Calculator design of type Calculator called calc
+
+
+        //We can simply call created object
+        int result = calc.add(num1, num2);
+        System.out.println("Result: " + result);
+
+
+
+    }
+}
--- Day 6/oopintroduction.java
+++
@@ -1,8 +0,0 @@
-public class oopintroduction
-{
-    public static void main(String args[])
-    {
-
-
-    }
-}
